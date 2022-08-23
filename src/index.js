cd2f94c3f29717f9cca4e8fa4109fe8cfb229fbc
Santiago Pujol <santiagopujol7@gmail.com>
2022-08-23 21:13:02 +0100

    Add markers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import * as L from 'leaflet';
 import style from './style.css';
+import * as data from './data.json';
 
-const map = L.map('map').setView([0, 0], 14);
+
+
+const map = L.map('map').setView([40, -5], 7);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+//render markers 
+for (let i = 0; i < 100; i++) {
+    L.marker([data[i].lat, data[i].lon]).addTo(map);
+}
 
 console.log("Hello World!");
