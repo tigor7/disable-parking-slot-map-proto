efc6f4ca9a52c829a8c90d7ebab25a55a391bade
Santiago Pujol <santiagopujol7@gmail.com>
2022-08-25 10:20:01 +0100

    update icon image and fixed its position

--- src/index.js
+++ src/index.js
@@ -18,11 +18,13 @@ const map = L.map('map').setView([40, -5], 7);
 const disableIcon = L.icon({
     iconUrl: icon,
 
-    iconSize:     [38, 66], // size of the icon
+    iconSize:     [38, 60], // size of the icon
     // shadowSize:   [50, 64], // size of the shadow
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     // shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconAnchor: [19, 60],
+
+    popupAnchor:  [19, -60] // point from which the popup should open relative to the iconAnchor
 });
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
